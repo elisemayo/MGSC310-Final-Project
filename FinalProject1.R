8834206ea49018f0bda32e9e476997a11c34a343
justinesitton <57599795+justinesitton@users.noreply.github.com>
2019-11-14 11:49:50 -0800

    Update FinalProject1.R

--- FinalProject1.R
+++ FinalProject1.R
@@ -63,6 +63,63 @@ summary(spotify_data)
 
 # --------------------------------------------------------------------
 
+# test and train data sets 
+train_idx <- sample(1:nrow(spotify_data), size = 0.75 * nrow(spotify_data))
+spotify_train <- spotify_data %>% slice(train_idx)
+spotify_test <- spotify_data %>% slice(-train_idx)
+
+# at least 3 predictive models so we are doing 5 (bc we want an A)
+# linear regression
+mod1 <- lm(song_popularity ~ .,
+           data = spotify_data)
+summary(mod1)
+
+mod2 <- lm(song_popularity ~ instrumentalness + energy + loudne,
+           data = spotify_data)
+summary(mod2)
+
+mod3 <- lm(energy ~ acousticness + loudness + instrumentalness,
+           data = spotify_data)
+summary(mod3)
+
+# lasso
+lasso_fit <- cv.glmnet(song_popularity ~ .,
+                       alpha = 1, 
+                       nfolds = 10,
+                       data = spotify_train)
+plot(lasso_fit)
+
+# ridge regression
+ridge_fit <- cv.glmnet(song_popularity ~ . ,
+                       data = spotify_data,
+                       nfolds = 10,
+                       alpha = 0)
+ridge_fit$lambda.min
+ridge_fit$lambda.1se
+
+# randomForest
+rf_fit <- randomForest(song_popularity ~ .,
+                       data = spotify_data,
+                       type = classification,
+                       ntree = 1000,
+                       mtry = 3,
+                       importance = TRUE,
+                       localImp = TRUE)
+rf_fit
+
+# LOOCV
+preds_LOOCV <- rep(NA, nrow(spotify_train))
+num_rows <- nrow(spotify_train)
+LOOCV_mods <- list()
+for(i in 1:num_rows){
+  logit_mod <- glm(song_popularity ~ .,
+                   data = spotify_train %>% slice(-i), family = binomial)
+  preds_LOOCV[i] <- predict(logit_mod, spotify_train %>% slice(i),
+                            type = "response")
+}
+
+# --------------------------------------------------------------------
+
 # graphs
 cormat <- cor(spotify_data %>% select_if(is.numeric) %>% drop_na()) %>% 
   round(3)
@@ -90,3 +147,7 @@ ggplot(top_hits_df, aes(x = danceability, y = song_popularity)) +
 ggplot(top_hits_df, aes(x = energy, y = levels, fill = song_popularity)) +
   geom_density_ridges()
 # graph 5 
+
+# to do
+# gg animate?
+# sentiment analysis
