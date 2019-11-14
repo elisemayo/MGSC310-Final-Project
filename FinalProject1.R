780622b661e21fa88897698689c5c90ee99d98bf
justinesitton <57599795+justinesitton@users.noreply.github.com>
2019-11-14 10:55:44 -0800

    Update FinalProject1.R

--- FinalProject1.R
+++ FinalProject1.R
@@ -4,6 +4,8 @@ install.packages("sentimentr")
 library(sentimentr)
 install.packages("geniusr")
 library(geniusr)
+library(forcats)
+library(ggridges)
 # ------------------------------------------------------------------
 
 # data
@@ -21,30 +23,38 @@ spotify_data <- full_join(x = spotify_data,
 spotify_data <- spotify_data %>% distinct()
 # getting rid of duplicates
 
-spotify_data <- spotify_data %>% 
-  mutate(top_playlist = fct_infreq(playlist))
-table(spotify_data$playlist, spotify_data$top_playlist)
-levels(spotify_data$top_playlist)
-top_playlist <- spotify_data %>% group_by("All out 00s", "other")
-# reordering factor levels by frequency to see top playlists
+top_hits <- spotify_data %>% filter(song_popularity > 80) %>% 
+  group_by(playlist)
+  
+top_hits_levels <- fct_lump(top_hits$playlist, n = 9)
 
-top_hits <- spotify_data %>% filter(song_popularity > 80)
+top_hits_df <- data.frame(
+  top_hits,
+  levels = top_hits_levels
+)
 # finding the most popular songs
+#factor(top_hits$playlist)
+
+
+
+
+
+# reordering factor levels by frequency to see top playlists
 
 # sentiment stuff
 # scrape album lyrics
-lyrics <- map_df(tracklist$song_lyrics_url, get_lyrics_url)
+#lyrics <- map_df(tracklist$song_lyrics_url, get_lyrics_url)
 
 # counting negative / positive words
-sentiment <- lyrics %>%
-  unnest_tokens(word, line) %>%
+#sentiment <- lyrics %>%
+ # unnest_tokens(word, line) %>%
   # remove stop words
-  anti_join(stop_words) %>%
+  #anti_join(stop_words) %>%
   # join afinn score
-  inner_join(bing) %>%
+#  inner_join(bing) %>%
   # count negative / positive words
-  count(word, sentiment, sort = TRUE) %>%
-  ungroup()
+ # count(word, sentiment, sort = TRUE) %>%
+#  ungroup()
 
 # --------------------------------------------------------------------
 
@@ -72,12 +82,11 @@ ggplot(spotify_data, aes(x = danceability, y = song_popularity)) +
   geom_smooth()
 # graph 3
 
-ggplot(spotify_data, aes(x = danceability, y = song_popularity)) + 
-  geom_point() + facet_wrap(~top_playlist) +
+ggplot(top_hits_df, aes(x = danceability, y = song_popularity)) + 
+  geom_point() + facet_wrap(~levels) +
   geom_abline(intercept = 0, slope = 1, color = "red", linetype = "dashed")
 # graph 4 
 
-ggplot(spotify_data, aes(x = energy, y = song_popularity, fill = top_playlist)) +
+ggplot(top_hits_df, aes(x = energy, y = levels, fill = song_popularity)) +
   geom_density_ridges()
 # graph 5 
-
